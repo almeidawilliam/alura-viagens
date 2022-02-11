e5bf153d435a4e0d795a93e4df676b274693bf6a
William Almeida <almeida.ws@hotmail.com>
2022-02-11 16:53:22 -0300

    class 2.6 (2nd course) - Refatorando o código Java

--- app/src/main/java/br/com/alura/aluraviagens/ui/activity/PackageHolidaySummaryActivity.kt
+++ app/src/main/java/br/com/alura/aluraviagens/ui/activity/PackageHolidaySummaryActivity.kt
@@ -12,37 +12,29 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 class PackageHolidaySummaryActivity : AppCompatActivity() {
+
+    companion object {
+        private const val APPBAR_TITLE = "Resumo do pacote"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_package_holiday_summary)
-        setTitle("Resumo do pacote")
+        title = (APPBAR_TITLE)
 
         val packageHoliday = PackageHoliday("São Paulo", "sao_paulo_sp", 2, BigDecimal(243.99))
 
-        findViewById<TextView>(R.id.activity_package_holiday_summary_place)
-            .apply {
-                this.text = packageHoliday.place
-            }
+        showPlace(packageHoliday)
+        showImage(packageHoliday)
+        showDays(packageHoliday)
+        showAmount(packageHoliday)
+        showDates(packageHoliday)
+    }
 
-        val drawable = getDrawable(this, packageHoliday.image)
-        findViewById<ImageView>(R.id.activity_package_holiday_summary_image)
-            .apply {
-                this.setImageDrawable(drawable)
-            }
-
-        findViewById<TextView>(R.id.activity_package_holiday_summary_days)
-            .apply {
-                this.text = packageHoliday.formattedDays()
-            }
-
-        findViewById<TextView>(R.id.activity_package_holiday_summary_amount)
-            .apply {
-                this.text = packageHoliday.formattedAmount()
-            }
-
+    private fun showDates(packageHoliday: PackageHoliday) {
         val brazilianFormat = SimpleDateFormat("dd/MM")
         val departureDate = Calendar.getInstance()
-        val returnDate: Calendar = Calendar.getInstance()
+        val returnDate = Calendar.getInstance()
         returnDate.add(Calendar.DATE, packageHoliday.days)
         val formattedDepartureDate: String = brazilianFormat.format(departureDate.time)
         val formattedReturnDate = brazilianFormat.format(returnDate.time)
@@ -52,6 +44,34 @@ class PackageHolidaySummaryActivity : AppCompatActivity() {
                 this.text =
                     "$formattedDepartureDate - $formattedReturnDate de ${returnDate.get(Calendar.YEAR)}"
             }
+    }
 
+    private fun showAmount(packageHoliday: PackageHoliday) {
+        findViewById<TextView>(R.id.activity_package_holiday_summary_amount)
+            .apply {
+                this.text = packageHoliday.formattedAmount()
+            }
+    }
+
+    private fun showDays(packageHoliday: PackageHoliday) {
+        findViewById<TextView>(R.id.activity_package_holiday_summary_days)
+            .apply {
+                this.text = packageHoliday.formattedDays()
+            }
+    }
+
+    private fun showImage(packageHoliday: PackageHoliday) {
+        val drawable = getDrawable(this, packageHoliday.image)
+        findViewById<ImageView>(R.id.activity_package_holiday_summary_image)
+            .apply {
+                this.setImageDrawable(drawable)
+            }
+    }
+
+    private fun showPlace(packageHoliday: PackageHoliday) {
+        findViewById<TextView>(R.id.activity_package_holiday_summary_place)
+            .apply {
+                this.text = packageHoliday.place
+            }
     }
 }
