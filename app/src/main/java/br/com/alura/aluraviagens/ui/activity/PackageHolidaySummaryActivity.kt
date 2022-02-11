c1f2d5f3396dec6c1b1f97c886e6b2e20b1873e0
William Almeida <almeida.ws@hotmail.com>
2022-02-10 22:10:45 -0300

    class 2.1 (2nd course) - Adicionando a data da viagem e titulo

--- app/src/main/java/br/com/alura/aluraviagens/ui/activity/PackageHolidaySummaryActivity.kt
+++ app/src/main/java/br/com/alura/aluraviagens/ui/activity/PackageHolidaySummaryActivity.kt
@@ -8,11 +8,14 @@ import br.com.alura.aluraviagens.R
 import br.com.alura.aluraviagens.model.PackageHoliday
 import br.com.alura.aluraviagens.util.getDrawable
 import java.math.BigDecimal
+import java.text.SimpleDateFormat
+import java.util.*
 
 class PackageHolidaySummaryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_package_holiday_summary)
+        setTitle("Resumo do pacote")
 
         val packageHoliday = PackageHoliday("São Paulo", "sao_paulo_sp", 2, BigDecimal(243.99))
 
@@ -37,9 +40,17 @@ class PackageHolidaySummaryActivity : AppCompatActivity() {
                 this.text = packageHoliday.formattedAmount()
             }
 
+        val brazilianFormat = SimpleDateFormat("dd/MM")
+        val departureDate = Calendar.getInstance()
+        val returnDate: Calendar = Calendar.getInstance()
+        returnDate.add(Calendar.DATE, packageHoliday.days)
+        val formattedDepartureDate: String = brazilianFormat.format(departureDate.time)
+        val formattedReturnDate = brazilianFormat.format(returnDate.time)
+
         findViewById<TextView>(R.id.activity_package_holiday_summary_days_range)
             .apply {
-                this.text = ""
+                this.text =
+                    "$formattedDepartureDate - $formattedReturnDate de ${returnDate.get(Calendar.YEAR)}"
             }
 
     }
