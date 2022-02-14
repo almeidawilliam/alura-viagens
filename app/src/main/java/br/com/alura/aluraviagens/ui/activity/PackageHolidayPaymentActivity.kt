8e005704a24c13a0985238e0a3565f16548f8944
William Almeida <almeida.ws@hotmail.com>
2022-02-14 20:47:26 -0300

    class 4.1 (2nd course) - Criando a tela de resumo da compra

--- app/src/main/java/br/com/alura/aluraviagens/ui/activity/PackageHolidayPaymentActivity.kt
+++ app/src/main/java/br/com/alura/aluraviagens/ui/activity/PackageHolidayPaymentActivity.kt
@@ -1,12 +1,32 @@
 package br.com.alura.aluraviagens.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import br.com.alura.aluraviagens.R
+import br.com.alura.aluraviagens.model.PackageHoliday
+import java.math.BigDecimal
 
 class PackageHolidayPaymentActivity : AppCompatActivity() {
+
+    companion object {
+        private const val APPBAR_TITLE = "PAGAMENTO"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_package_holiday_payment)
+        title = APPBAR_TITLE
+        val packageHoliday = PackageHoliday("São Paulo", "sao_paulo_sp", 2, BigDecimal(243.99))
+        showAmount(packageHoliday)
+
+        val intent = Intent(this@PackageHolidayPaymentActivity, PurchaseSummaryActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun showAmount(packageHoliday: PackageHoliday) {
+        val amount = findViewById<TextView>(R.id.activity_package_holiday_payment_amount)
+        amount.text = packageHoliday.formattedAmount()
     }
 }
---
+++ app/src/main/java/br/com/alura/aluraviagens/ui/activity/PurchaseSummaryActivity.kt
@@ -0,0 +1,13 @@
+package br.com.alura.aluraviagens.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.alura.aluraviagens.R
+
+class PurchaseSummaryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_purchase_summary)
+    }
+}
